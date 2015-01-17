a78677b22e77454af35e4a6cbca64c14fde63d47
Sasha Babicki <sbabicki@gmail.com>
2015-01-16 21:53:12 -0700

    first commit. serve only index page

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import SocketServer
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 # 
-#     http://www.apache.org/licenses/LICENSE-2.0
+#	 http://www.apache.org/licenses/LICENSE-2.0
 # 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,7 +17,7 @@ import SocketServer
 #
 #
 # Furthermore it is derived from the Python documentation examples thus
-# some of the code is Copyright © 2001-2013 Python Software
+# some of the code is Copyright  2001-2013 Python Software
 # Foundation; All Rights Reserved
 #
 # http://docs.python.org/2/library/socketserver.html
@@ -28,19 +28,26 @@ import SocketServer
 
 
 class MyWebServer(SocketServer.BaseRequestHandler):
-    
-    def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+
+	def handle(self):
+		self.data = self.request.recv(1024).strip()
+		print ("Got a request of: %s\n" % self.data)
+		#self.request.sendall("OK")
+		#self.request.sendall(self.data.upper())
+		f = open("./www/index.html", "r+")
+		self.request.sendall(f.read())
+		
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 8080
+	HOST, PORT = "localhost", 8080
 
-    SocketServer.TCPServer.allow_reuse_address = True
-    # Create the server, binding to localhost on port 8080
-    server = SocketServer.TCPServer((HOST, PORT), MyWebServer)
+	SocketServer.TCPServer.allow_reuse_address = True
+	# Create the server, binding to localhost on port 8080
+	server = SocketServer.TCPServer((HOST, PORT), MyWebServer)
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    server.serve_forever()
+	# Activate the server; this will keep running until you
+	# interrupt the program with Ctrl-C
+	server.serve_forever()
+	
+
+
